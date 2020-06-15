f6ab0265d3c488d5d37f69875f8a64355bbcf63c
DanielM24 <simionovmdaniel10@gmail.com>
2020-06-15 19:29:06 +0300

    Speech to Text

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import speech_recognition as sr
+import json
 
 def main():
     print("Incercati sa rostiti ceva: ")
     recognizer = sr.Recognizer()
-
     with sr.Microphone() as source:
         speech_to_text = recognizer.listen(source)
 
         recognizer.adjust_for_ambient_noise(source)
     try:
         text = recognizer.recognize_google(speech_to_text, language="ro-RO")
-
-        print('\n Conversie efectuata cu succes: {} ' . format(text))
-
+        data = {"speech_to_text": "{}".format(text)}
     except:
         print("Mai incercati inca o data.")
 
-    with open("sample.wav", "wb") as sample:
-        sample.write(speech_to_text.get_wav_data())
+    with open("data.json", "w") as sample:
+        json.dump(data, sample)
 
 if __name__ =="__main__":
     main()
